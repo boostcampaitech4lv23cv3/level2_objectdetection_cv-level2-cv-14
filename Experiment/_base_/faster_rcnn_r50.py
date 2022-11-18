5539ec0943e4ba00db458a4c6dcdafbe2609db44
GeonHyeock <gunheock@gmail.com>
2022-11-18 06:27:09 +0000

    base

---
+++ Experiment/_base_/faster_rcnn_r50.py
@@ -0,0 +1,7 @@
+_base_ = [
+    "../../Experiment/_base_/datasets/coco_detection.py",
+    "../../Experiment/_base_/models/yolov3_mobilenetv2_mstrain-416_300e_coco.py",
+    "../../Experiment/_base_/schedules/schedule_1x.py",
+    "../../Experiment/_base_/default_runtime.py"
+]
+
