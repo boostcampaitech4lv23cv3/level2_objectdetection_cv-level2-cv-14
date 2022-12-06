39ea8f7d205ef6e509e0405f8d60b9da82e5ea30
GeonHyeock <gunheock@gmail.com>
2022-12-06 11:00:38 +0900

    Remove & Rename
    dev -> master branch로 merge하기 위한 README 작성 및 파일 삭제

--- Experiment/cascade_rcnn_pafpn_convnext-t/schedules/schedule_adamw_1x.py
+++
@@ -1,9 +0,0 @@
-optimizer = dict(
-    type="Adam",
-    lr=0.0001,
-    betas=(0.9, 0.999))
-optimizer_config = dict(grad_clip=None)
-lr_config = dict(
-    policy="step", warmup="linear", warmup_iters=50, warmup_ratio=0.0001, step=[4, 11]
-)
-runner = dict(type="EpochBasedRunner", max_epochs=12)
--- Experiment/yolov3/schedules/schedule_1x.py
+++
@@ -1,8 +0,0 @@
-# optimizer
-optimizer = dict(type="Adam", lr=0.00001, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
-# learning policy
-lr_config = dict(
-    policy="step", warmup="linear", warmup_iters=2000, warmup_ratio=0.1, step=[218, 246]
-)
-runner = dict(type="EpochBasedRunner", max_epochs=45)
